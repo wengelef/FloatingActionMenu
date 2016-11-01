c5596eced67eeead7925eb6c1606cb0f5f40723b
Flo <wengelef@gmail.com>
2016-11-01 19:14:02 +0100

    feature (FloatingActionMenu test): adds TestProject implementation

--- app/src/main/java/com/wengelef/floatingactionmenu/app/MainActivity.java
+++ app/src/main/java/com/wengelef/floatingactionmenu/app/MainActivity.java
@@ -2,12 +2,32 @@ package com.wengelef.floatingactionmenu.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.wengelef.floatingactionmenu.FloatingActionMenu;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.wengelef.floatingactionmenu.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionMenu mFloatingActionMenu = (FloatingActionMenu) findViewById(R.id.fab_menu);
+
+        mFloatingActionMenu.addFab(new FloatingActionMenu.MenuAction(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("FloatingActionMenu", "play clicked!");
+            }
+        }, R.drawable.ic_play_arrow_white_24dp));
+
+        mFloatingActionMenu.addFab(new FloatingActionMenu.MenuAction(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("FloatingActionMenu", "pause clicked!");
+            }
+        }, R.drawable.ic_pause_white_24dp));
     }
 }
