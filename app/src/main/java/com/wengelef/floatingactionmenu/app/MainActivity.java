e836db4da145883b9f4190ead1f79038adbfc6a9
Flo <wengelef@gmail.com>
2016-11-01 19:10:09 +0100

    chore (resources): adds icons

--- app/src/main/java/com/wengelef/floatingactionmenu/app/MainActivity.java
+++ app/src/main/java/com/wengelef/floatingactionmenu/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.wengelef.floatingactionmenu;
+package com.wengelef.floatingactionmenu.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(com.wengelef.floatingactionmenu.R.layout.activity_main);
     }
 }
---
+++ library/src/main/java/com/wengelef/floatingactionmenu/FloatingActionMenu.java
@@ -0,0 +1,8 @@
+package com.wengelef.floatingactionmenu;
+
+/**
+ * Created by Flo on 01.11.2016.
+ */
+
+public class FloatingActionMenu {
+}
